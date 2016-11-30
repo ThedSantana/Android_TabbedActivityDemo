cb2b6fd91a144839c9a5d3ebcb7733715d478a6a
Jason Butwell <jasonbutwell@gmail.com>
2016-11-30 14:15:36 +0000

    Started implementing the xml layout files and fragment classes for each of the tabbed content pages.

---
+++ app/src/main/java/com/jasonbutwell/tabbedactivitydemo/Tab1.java
@@ -0,0 +1,20 @@
+package com.jasonbutwell.tabbedactivitydemo;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by J on 30/11/2016.
+ */
+
+public class Tab1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1, container, false);
+
+        return rootView;
+    }
+}
