3edfeb6810b74e6471658bee2dfc79f8db574f0c
Jason Butwell <jasonbutwell@gmail.com>
2016-11-30 15:18:26 +0000

    Improved charSequence method which returns the names of the tabs. Replaced switch-case with simple array index to access a String array containing the names of the tabs.

--- app/src/main/java/com/jasonbutwell/tabbedactivitydemo/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/tabbedactivitydemo/MainActivity.java
@@ -1,25 +1,23 @@
 package com.jasonbutwell.tabbedactivitydemo;
 
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String []tabNames = { "Home", "About", "Services","Testimonials", "Contact" };
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -65,7 +63,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -89,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
+    // Deleted PlaceHolderFragment
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -103,63 +100,51 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+
+            Log.i("position ",""+position);
+
+            switch (position) {
+
+                case 0:
+                    Tab1 tab1 = new Tab1();
+                    return tab1;
+
+                case 1:
+                    Tab2 tab2 = new Tab2();
+                    return tab2;
+
+                case 2:
+                    Tab3 tab3 = new Tab3();
+                    return tab3;
+
+                case 3:
+                    Tab4 tab4 = new Tab4();
+                    return tab4;
+
+                case 4:
+                    Tab5 tab5 = new Tab5();
+                    return tab5;
+
+                default:
+                    return null;
+
+            }
         }
 
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return 5;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
-            }
-            return null;
+            
+            if ( position < tabNames.length )
+                return tabNames[position];
+            else
+                return null;
         }
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.tab1, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
 }
--- app/src/main/java/com/jasonbutwell/tabbedactivitydemo/Tab2.java
+++ app/src/main/java/com/jasonbutwell/tabbedactivitydemo/Tab2.java
@@ -1,6 +1,6 @@
 package com.jasonbutwell.tabbedactivitydemo;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +10,7 @@ import android.view.ViewGroup;
  * Created by J on 30/11/2016.
  */
 
-public class Tab2  extends Fragment {
+public class Tab2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab2, container, false);
